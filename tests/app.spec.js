80a0092307157c83f9fd48489d6125f4cf008bf9
MateuszC100 <mateuszcierniak102@o2.pl>
2017-04-12 00:27:22 +0200

    Add tests to generateMessage() function (without sinon.spy and sinon.stub)

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,11 +1,52 @@
 'use strict';
 
 var app = require('../app/app');
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 
 describe('generateMessage without sinon.spy and sinon.stub', function ()
 {
+    describe("When parameter is ok", function(){
 
+        describe("and parameter is a palindrome", function(){
+            describe("and has some vovels", function(){
+                it("should return object with message, which contains info about being palindrome and vovel count > 0", function(){
+                    expect(app.generateMessage('zaraz')).to.eql({vowel: 2, palindrome: true, message: "zaraz is palindrome and has 2 vovels"});
+                });
+            });
+            describe("and has no vovels", function(){
+                it("should return object with message, which contains info about being palindrome and vovel count = 0", function(){
+                    expect(app.generateMessage('dppd')).to.eql({vowel: 0, palindrome: true, message: "dppd is palindrome and has no vovels"});
+                });
+
+            });
+        });
+
+        describe("and parameter is not a palindrome", function(){
+            describe("and has some vovels", function(){
+                it("should return object with message, which contains info about not being palindrome and vovel count > 0", function(){
+                    expect(app.generateMessage('aab')).to.eql({vowel: 2, palindrome: false, message: "aab is not palindrome and has 2 vovels"});
+                });
+            });
+            describe("and has no vovels", function(){
+                it("should return object with message, which contains info about not being palindrome and vovel count = 0", function(){
+                    expect(app.generateMessage('dppp')).to.eql({vowel: 0, palindrome: false, message: "dppp is not palindrome and has no vovels"});
+                });
+
+            });
+        });
+    });
+
+    describe("When parameter is not ok", function(){
+        it("should throw Error('Argument is undefined, not a string or empty string!)'", function(){
+            expect(function(){app.generateMessage(123)}).to.throw('Argument is undefined, not a string or empty string!');
+        });
+    });
 });
+
 
 describe('generateMessage with sinon.spy adn sinon.stub', function ()
 {
